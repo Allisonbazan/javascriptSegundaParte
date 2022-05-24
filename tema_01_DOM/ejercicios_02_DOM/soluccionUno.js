9152c0a12a87f5f89ccc574a52e777bbc069efbb
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2022-05-23 19:22:32 -0500

    Revision del ejercicio

--- tema_01_DOM/ejercicios_02_DOM/soluccionUno.js
+++ tema_01_DOM/ejercicios_02_DOM/soluccionUno.js
@@ -5,11 +5,22 @@ const actionTwo = () => {
     document.getElementById('divTwo').style.width = '600px';
 }
 
-
 const actionThree = () => {
-    let divSelect = document.getElementById('divThree');
+
+    // primera forma
+    // let divSelect = document.getElementById('divThree');
+
+    // divSelect.style.border = '10px';
+    // divSelect.style.borderStyle = 'dotted';
+    // divSelect.style.borderColor = '#870000';
+
+    // Segunda forma
     // divSelect.style.border = '10px dotted #870000'
-    divSelect.style.border = '10px';
-    divSelect.style.borderStyle = 'dotted';
-    divSelect.style.borderColor = '#870000';
+
+    // Tercera forma
+    document.getElementById('divThree').style.border = '10px';
+    document.getElementById('divThree').style.borderStyle = 'dotted';
+    document.getElementById('divThree').style.borderColor = '#870000';
+
+
 }
