8b285d8419d51ab2848bc34a856df70b09d58330
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2022-05-23 19:38:47 -0500

    Soluccion 1

--- Reto de la semana/tablas de multiplicar/soluction1/ejemplo.js
+++ Reto de la semana/tablas de multiplicar/soluction1/ejemplo.js
@@ -1,26 +1,29 @@
 let tablas = () => {
     // Tabla 1
-    for (let ciclo = 1; ciclo <= 10; ciclo++) {
+    for (let ciclo = 1; ciclo <= 5; ciclo++) {
+        //++ autoincrementable    ciclo++   ciclo=ciclo +1
         let tabla = 1;
         let texth1 = document.createElement("tr");
         let resultado = ciclo * tabla;
-        texth1.innerHTML = `<th>${tabla}*${ciclo}</th><th>=${resultado}</th>`;
+        texth1.innerHTML = `<td>${tabla}*${ciclo}</td><td>=${resultado}</td>`;
         document.getElementsByTagName("tbody")[0].appendChild(texth1);
     }
     // Tabla 2
-    for (let ciclo = 1; ciclo <= 10; ciclo++) {
+    for (let ciclo = 3; ciclo <= 10; ciclo++) {
         let tabla = 2;
         let texth1 = document.createElement("tr");
         let resultado = ciclo * tabla;
-        texth1.innerHTML = `<th>${tabla}*${ciclo}</th><th>=${resultado}</th>`;
+        texth1.innerHTML = `<td>${tabla}*${ciclo}</td><td>=${resultado}</td>`;
         document.getElementsByTagName("tbody")[1].appendChild(texth1);
+
     }
+    
     // Tabla 3
     for (let ciclo = 1; ciclo <= 10; ciclo++) {
         let tabla = 3;
         let texth1 = document.createElement("tr");
         let resultado = ciclo * tabla;
-        texth1.innerHTML = `<th>${tabla}*${ciclo}</th><th>=${resultado}</th>`;
+        texth1.innerHTML = `<td>${tabla}*${ciclo}</td><td>=${resultado}</td>`;
         document.getElementsByTagName("tbody")[2].appendChild(texth1);
     }
     // Tabla 4
@@ -28,7 +31,7 @@ let tablas = () => {
         let tabla = 4;
         let texth1 = document.createElement("tr");
         let resultado = ciclo * tabla;
-        texth1.innerHTML = `<th>${tabla}*${ciclo}</th><th>=${resultado}</th>`;
+        texth1.innerHTML = `<td>${tabla}*${ciclo}</td><td>=${resultado}</td>`;
         document.getElementsByTagName("tbody")[3].appendChild(texth1);
     }
     // Tabla 5
@@ -36,7 +39,7 @@ let tablas = () => {
         let tabla = 5;
         let texth1 = document.createElement("tr");
         let resultado = ciclo * tabla;
-        texth1.innerHTML = `<th>${tabla}*${ciclo}</th><th>=${resultado}</th>`;
+        texth1.innerHTML = `<td>${tabla}*${ciclo}</td><td>=${resultado}</td>`;
         document.getElementsByTagName("tbody")[4].appendChild(texth1);
     }
     // Tabla 6
@@ -44,7 +47,7 @@ let tablas = () => {
         let tabla = 6;
         let texth1 = document.createElement("tr");
         let resultado = ciclo * tabla;
-        texth1.innerHTML = `<th>${tabla}*${ciclo}</th><th>=${resultado}</th>`;
+        texth1.innerHTML = `<td>${tabla}*${ciclo}</td><td>=${resultado}</td>`;
         document.getElementsByTagName("tbody")[5].appendChild(texth1);
     }
     // Tabla 7
@@ -52,7 +55,7 @@ let tablas = () => {
         let tabla = 7;
         let texth1 = document.createElement("tr");
         let resultado = ciclo * tabla;
-        texth1.innerHTML = `<th>${tabla}*${ciclo}</th><th>=${resultado}</th>`;
+        texth1.innerHTML = `<td>${tabla}*${ciclo}</td><td>=${resultado}</td>`;
         document.getElementsByTagName("tbody")[6].appendChild(texth1);
     }
     // Tabla 8
@@ -60,7 +63,7 @@ let tablas = () => {
         let tabla = 8;
         let texth1 = document.createElement("tr");
         let resultado = ciclo * tabla;
-        texth1.innerHTML = `<th>${tabla}*${ciclo}</th><th>=${resultado}</th>`;
+        texth1.innerHTML = `<td>${tabla}*${ciclo}</td><td>=${resultado}</td>`;
         document.getElementsByTagName("tbody")[7].appendChild(texth1);
     }
     // Tabla 9
@@ -68,7 +71,7 @@ let tablas = () => {
         let tabla = 9;
         let texth1 = document.createElement("tr");
         let resultado = ciclo * tabla;
-        texth1.innerHTML = `<th>${tabla}*${ciclo}</th><th>=${resultado}</th>`;
+        texth1.innerHTML = `<td>${tabla}*${ciclo}</td><td>=${resultado}</td>`;
         document.getElementsByTagName("tbody")[8].appendChild(texth1);
     }
     // Tabla 10
@@ -76,8 +79,8 @@ let tablas = () => {
         let tabla = 10;
         let texth1 = document.createElement("tr");
         let resultado = ciclo * tabla;
-        texth1.innerHTML = `<th>${tabla}*${ciclo}</th><th>=${resultado}</th>`;
+        texth1.innerHTML = `<td>${tabla}*${ciclo}</td><td>=${resultado}</td>`;
         document.getElementsByTagName("tbody")[9].appendChild(texth1);
     }
+    
 }
-tablas();
